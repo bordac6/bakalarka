b79d67505a1f6180bebc2f47ea08bd36685876ef
Tomas Bordac <bordac6@uniba.sk>
2018-01-03 15:05:24 +0100

    openPresentationIntent added

--- desktop/client.js
+++ desktop/client.js
@@ -1,9 +1,13 @@
+//import { spawn } from 'child_process';
+
 var http = require('http'),
     querystring = require('querystring')
     io = require('socket.io-client'),
     ks = require('node-key-sender'),
+    finder = require('fs-finder'),
+    exec = require('child_process').exec,
     responseToAlexa = ""
-
+    
 socket = io('https://195d3fa9.ngrok.io')
 socket.on('connect', () => {
     console.log('is connected')
@@ -19,8 +23,23 @@ setInterval(() => {
             var jsonData = JSON.parse(res);
             if(jsonData.request.type == "IntentRequest"){
                 if(jsonData.request.intent.name == "NextSlideIntent"){
-                    ks.sendKey(['right'])
+                    ks.sendKey('right')
                     response("should be here some JSON data")
+                }
+                else if(jsonData.request.intent.name == "OpenPresentationIntent"){
+                    var presentationName = jsonData.request.intent.slots.FileName.value + ".pptx"
+                    console.log(presentationName)
+                    var file = finder.from('').findFirst().findFiles(presentationName)
+                    console.log(file)
+                    var yourscript = exec(file, (error, stdout, stderr) => {
+                        ks.sendKey('f5')
+                        if(error !== null){
+                            console.log(`exec error: ${error}`)
+                        }
+                    })
+                }
+                else if(jsonData.request.intent.name == "StartPresentationIntent"){
+                    ks.sendKey('f5')
                 }
                 else{
                     response("Intent does not exist.")
@@ -35,3 +54,34 @@ function response(msg){
         console.log('Recieved from server: '+res)
     })
 }
+
+/*
+ks.sendCombination(['control', 'alt', 'delete'])
+setInterval(()=>{
+    ks.sendKey(['f5'])
+    ks.sendKey(['right'])
+}, 5000)
+*/
+//HTTP POST request
+/*
+var postData = querystring.stringify({
+    msg: "give me JSON from Alexa!!\n"
+})
+var post_option = {
+    host: 'e69818e1.ngrok.io',
+    path: '/bordac6',
+    method: 'POST'
+}
+var req = http.request(post_option, (res) => {
+    res.setEncoding('utf8')
+    res.on('data', (data) => {
+        console.log('Respose: ' + data)
+    })
+    res.on('end', () => {
+        console.log('No more data in response.')
+    })
+})
+
+req.write(postData)
+req.end()
+*/
