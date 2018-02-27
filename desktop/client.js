273db1586692b07c6f7dea2ac97879137a79b043
Tomas Bordac <bordac6@uniba.sk>
2018-02-27 21:51:22 +0100

    response to Alexa from client

--- desktop/client.js
+++ desktop/client.js
@@ -1,67 +1,145 @@
 //import { spawn } from 'child_process';
 
-var http = require('http'),
-    querystring = require('querystring')
-    io = require('socket.io-client'),
-    ks = require('node-key-sender'),
-    finder = require('fs-finder'),
-    exec = require('child_process').exec,
-    os = process.platform,
-    responseToAlexa = ""
+var http = require('http')
+var querystring = require('querystring')
+var io = require('socket.io-client')
+var ks = require('node-key-sender')
+var finder = require('fs-finder')
+var exec = require('child_process').exec
+var os = process.platform
+var responseToAlexa = ""
     
-socket = io('https://2992d795.ngrok.io')
+socket = io('https://65fd70f2.ngrok.io')
 socket.on('connect', () => {
     console.log('is connected')
+    socket.emit('room', 'test', 'name', 'passwd', (msg) => {
+        console.log('is connected? -', msg)
+    })
+})
+socket.on('message', (requestBody) => {
+    //console.log('Incoming message:', requestBody)
+
+    var jsonData = JSON.parse(requestBody)
+    var type = jsonData.request.type
+
+    craftResponse(type, jsonData)
+    if(type === "IntentRequest")
+        executeCommand(jsonData)
+    
+   
 })
 socket.on('event', (data) => {})
 socket.on('disconnect', () => {})
-
-setInterval(() => {
-    socket.emit('task', 'bordac6', (res) => {
-        //console.log(`Recieve:\n${res}`)
-        if(res != ""){
-            
-            var jsonData = JSON.parse(res);
-            if(jsonData.request.type == "IntentRequest"){
-                if(jsonData.request.intent.name == "NextSlideIntent"){
-                    ks.sendKey('right')
-                    response("should be here some JSON data")
-                }
-                else if(jsonData.request.intent.name == "OpenPresentationIntent"){
-                    var presentationName = jsonData.request.intent.slots.FileName.value + ".pptx"
-                    console.log(presentationName)
-                    var file = finder.from('').findFirst().findFiles(presentationName)
-                    console.log(file)
-		    if(file !== null){
-		   	 if(os == 'linux'){
-  				exec('xdg-open ' + file, (err, out, code) => {
-			    	    ks.sendKey('f5')
-			    	    if(err) throw err
-			  	})
-		    	  }
-		    	  else if(os == 'win32'){ 
-                              exec(file, (error, stdout, stderr) => {
-                                  ks.sendKey('f5')
-                                  if(error !== null){
-                                      console.log(`exec error: ${error}`)
-                                  }
-                              })
-		    	  }
-                    }        
-		}
-                else if(jsonData.request.intent.name == "StartPresentationIntent"){
-                    ks.sendKey('f5')
-                }
-                else{
-                    response("Intent does not exist.")
-                }
-            }
-        }
-    })
-}, 3000)
 
 function response(msg){
     socket.emit('alexaRes', msg, (res) => {
         console.log('Recieved from server: '+res)
     })
+}
+function craftResponse(type, data){
+    if(type === "LaunchRequest") {
+      responseBody = {
+        "version": "0.1",
+        "response": {
+          "outputSpeech": {
+            "type": "PlainText",
+            "text": "Welcome to Echo Sample! Please say a command"
+          },
+          "card": {
+            "type": "Simple",
+            "title": "Opened",
+            "content": "You started the Node.js Echo API Sample"
+          },
+          "reprompt": {
+            "outputSpeech": {
+              "type": "PlainText",
+              "text": "Say a command"
+            }
+          },
+          "shouldEndSession": false
+        }
+      };
+    }
+    else if(type === "IntentRequest") {
+      // crafting a response
+      responseBody = {
+        "version": "0.1",
+        "response": {
+          "outputSpeech": {
+            "type": "PlainText",
+            "text": "Do somethink on computer."
+          },
+          "card": {
+            "type": "Simple",
+            "title": "DoCommand",
+            "content": "You sent command to Node.js client"
+          },
+          "reprompt": {
+            "outputSpeech": {
+              "type": "PlainText",
+              "text": "Say a command"
+            }
+          },
+          "shouldEndSession": false
+        }
+      };
+    }
+    else{
+        responseBody = {
+            "version": "0.1",
+            "response": {
+              "outputSpeech": {
+                "type": "PlainText",
+                "text": "Command sent to client."
+              },
+              "card": {
+                "type": "Simple",
+                "title": "Error Parsing",
+                "content": JSON.stringify(data)
+              },
+              "reprompt": {
+                "outputSpeech": {
+                  "type": "PlainText",
+                  "text": "Say a command"
+                }
+              },
+              "shouldEndSession": true
+            }
+        }
+    }
+    response(responseBody)
+}
+function executeCommand(jsonData){
+        if(jsonData.request.intent.name == "NextSlideIntent"){
+            ks.sendKey('right')
+            response("should be here some JSON data")
+        }
+        else if(jsonData.request.intent.name == "OpenPresentationIntent"){
+            var presentationName = jsonData.request.intent.slots.FileName.value + ".pptx"
+            console.log(presentationName)
+            var file = finder.from('').findFirst().findFiles(presentationName)
+            console.log(file)
+            if(file !== null){
+                if(os == 'linux'){
+                    exec('xdg-open ' + file, (err, out, code) => {
+                        ks.sendKey('f5')
+                        if(err) throw err
+                    })
+                }
+                else if(os == 'win32'){ 
+                    exec(file, (error, stdout, stderr) => {
+                        ks.sendKey('f5')
+                        if(error !== null){
+                            console.log(`exec error: ${error}`)
+                        }
+                    })
+                }
+            }        
+        }
+        else if(jsonData.request.intent.name == "StartPresentationIntent"){
+            ks.sendKey('f5')
+        }
+        else{
+            response("Intent does not exist.")
+        }
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    port = process.env.PORT || 3000,
-    alexaReq = "",
-    alexaRes = {},
-    io = require('socket.io').listen(server)
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+var port = process.env.PORT || 3000
+var alexaRes = ''
+var responseToAlexa = {}
+var clients = []
 
 // Creates the website server on the port #
 server.listen(port, function () {
@@ -15,7 +16,6 @@ server.listen(port, function () {
 app.post('/api/echo', function(req, res){
   console.log("received echo request");
   var requestBody = "";
-
   // Will accumulate the data
   req.on('data', function(data){
     requestBody+=data;
@@ -25,82 +25,92 @@ app.post('/api/echo', function(req, res){
   req.on('end', function(){
 
     //send data to client
-    alexaReq = requestBody
-    
-    var jsonData = JSON.parse(alexaReq)
+    io.to(clients[0]).emit('message', requestBody)
 
-    if(jsonData.request.type == "LaunchRequest")
-    {
-      // crafting a response
-      responseBody = {
-        "version": "0.1",
-        "response": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Welcome to Echo Sample! Please say a command"
-          },
-          "card": {
-            "type": "Simple",
-            "title": "Opened",
-            "content": "You started the Node.js Echo API Sample"
-          },
-          "reprompt": {
-            "outputSpeech": {
-              "type": "PlainText",
-              "text": "Say a command"
-            }
-          },
-          "shouldEndSession": false
-        }
-      };
-    }
-    else{
-        responseBody = {
-            "version": "0.1",
-            "response": {
-              "outputSpeech": {
-                "type": "PlainText",
-                "text": "Command sent to client."
-              },
-              "card": {
-                "type": "Simple",
-                "title": "Error Parsing",
-                "content": JSON.stringify(requestBody)
-              },
-              "reprompt": {
-                "outputSpeech": {
-                  "type": "PlainText",
-                  "text": "Say a command"
-                }
-              },
-              "shouldEndSession": false
-            }
-        }
-    }
-    
-    res.statusCode = 200;
-    res.contentType('application/json');
-    res.send(responseBody);
+    // var jsonData = JSON.parse(requestBody)
 
+    // if(jsonData.request.type == "LaunchRequest")
+    // {
+    //   // crafting a response
+    //   responseBody = {
+    //     "version": "0.1",
+    //     "response": {
+    //       "outputSpeech": {
+    //         "type": "PlainText",
+    //         "text": "Welcome to Echo Sample! Please say a command"
+    //       },
+    //       "card": {
+    //         "type": "Simple",
+    //         "title": "Opened",
+    //         "content": "You started the Node.js Echo API Sample"
+    //       },
+    //       "reprompt": {
+    //         "outputSpeech": {
+    //           "type": "PlainText",
+    //           "text": "Say a command"
+    //         }
+    //       },
+    //       "shouldEndSession": false
+    //     }
+    //   };
+    // }
+    // else{
+    //     responseBody = {
+    //         "version": "0.1",
+    //         "response": {
+    //           "outputSpeech": {
+    //             "type": "PlainText",
+    //             "text": "Command sent to client."
+    //           },
+    //           "card": {
+    //             "type": "Simple",
+    //             "title": "Error Parsing",
+    //             "content": JSON.stringify(requestBody)
+    //           },
+    //           "reprompt": {
+    //             "outputSpeech": {
+    //               "type": "PlainText",
+    //               "text": "Say a command"
+    //             }
+    //           },
+    //           "shouldEndSession": false
+    //         }
+    //     }
+    // }
+      setTimeout(() => {
+        console.log("ALEXA RESPONSE 82: ", alexaRes)
+        res.statusCode = 200;
+        res.contentType('application/json');
+        res.send(alexaRes);
+        alexaRes = '';
+      }, 500)    
   })
 })
 
 //socket.io
 io.on('connect', (socket) => {
     console.log('New client on socket is connected.')
-    
+
+    socket.on('room', (room, name, passwd, callback) => {
+      console.log(name, ' ', passwd)
+      if(name === 'name' && passwd === 'passwd'){
+        socket.join(room)
+        clients.push(socket.id)
+        console.log(clients)
+        callback('yes')
+      }
+      else 
+        callback('no')
+    })
     socket.on('disconnect', () => {
+        var index = clients.indexOf(socket.id)
+        if(index > -1) clients.splice(index, 1)
         console.log('Client disconnected.')
+        console.log(clients)
     })
-
-    socket.on('task', (msg, req) => {
-        if(alexaReq != "") 
-            req(alexaReq)
-        alexaReq = ""
-    })
-
     socket.on('alexaRes', (req, res) => {
         alexaRes = req
+        console.log("ALEXA RESPONSE 112: ", alexaRes)
         res("OK")
     })
 })
